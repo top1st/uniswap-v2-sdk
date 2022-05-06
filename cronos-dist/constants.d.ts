d42bcc8fab3093c2776a017056fd488084437fd7
top1st <riguang102@gmail.com>
2022-05-07 02:46:43 +0800

    add cronos-dist

---
+++ cronos-dist/constants.d.ts
@@ -0,0 +1,9 @@
+import JSBI from 'jsbi';
+export declare const FACTORY_ADDRESS = "0xE98FaA8d9Fb2d65d8BF5aF407D3e5F3C7Eae397c";
+export declare const INIT_CODE_HASH = "0x697156375092f0dd365b9d117f6db5edec709e41183fea4d55663939c64829c2";
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const FIVE: JSBI;
+export declare const _997: JSBI;
+export declare const _1000: JSBI;
