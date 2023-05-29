f39c5f47db26e49383bd33b0560c66222e84e457
top1st <riguang102@gmail.com>
2023-05-29 23:29:26 +0800

    dist

---
+++ lightswap-dist/constants.d.ts
@@ -0,0 +1,9 @@
+import JSBI from 'jsbi';
+export declare const FACTORY_ADDRESS = "0xcD6fc1C421A79F777E6Bb76AA5E1eAb5991A0d8D";
+export declare const INIT_CODE_HASH = "0x540c9fbac3bf3431814da396bfc770ec61400eb56cccf36110c32b07ecc491b3";
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const FIVE: JSBI;
+export declare const _997: JSBI;
+export declare const _1000: JSBI;
